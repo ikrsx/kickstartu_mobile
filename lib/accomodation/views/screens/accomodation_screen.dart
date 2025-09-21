053d14a151ad8cf73e010fad0b4677ab3b10c68d
ikrsx <krishal.sh@tuta.io>
2025-09-21 15:58:31 +0530

    added accomodation feature

---
+++ lib/accomodation/views/screens/accomodation_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AccomodationScreen extends StatelessWidget {
+  const AccomodationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
